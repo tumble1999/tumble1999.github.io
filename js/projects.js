64cc4827b9e186e1ad868bbf7ffbff8e587b0f36
Tumble <tumblegamer@tumblenet.tk>
2016-12-02 14:06:30 +0000

    Create projects.js

---
+++ js/projects.js
@@ -0,0 +1,11 @@
+var mydiv=document.getElementById('github')
+
+//Using DOM's getAttribute() property
+var brand=mydiv.getAttribute("data-brand") //returns "toyota"
+mydiv.setAttribute("data-brand", "mazda") //changes "data-brand" to "mazda"
+mydiv.removeAttribute("data-brand") //removes "data-brand" attribute entirely
+
+//Using JavaScript's dataset property
+var brand=mydiv.dataset.brand //returns "toyota"
+mydiv.dataset.brand='mazda' //changes "data-brand" to "mazda"
+mydiv.dataset.brand=null //removes "data-brand" attribute

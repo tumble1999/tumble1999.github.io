5fd618b03ceea9f5749aa5acd9de1a5b6567996f
Tumble <tumblegamer@tumblenet.tk>
2016-12-06 19:34:42 +0000

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,35 +1,34 @@
 $(function() {
         $('div#github').each(function() {
-        ghdiv=this;
+                ghdiv=this;
         
-        var d=document.getElementById(ghdiv);
+                var d=document.getElementById(ghdiv);
 
-        /*
-        var mydiv=document.getElementById('githun')
-        //Using DOM's getAttribute() property
-        var brand=mydiv.getAttribute("data-brand") //returns "toyota"
-        mydiv.setAttribute("data-brand", "mazda") //changes "data-brand" to "mazda"
-        mydiv.removeAttribute("data-brand") //removes "data-brand" attribute entirely
+                /*
+                var mydiv=document.getElementById('githun')
+                //Using DOM's getAttribute() property
+                var brand=mydiv.getAttribute("data-brand") //returns "toyota"
+                mydiv.setAttribute("data-brand", "mazda") //changes "data-brand" to "mazda"
+                mydiv.removeAttribute("data-brand") //removes "data-brand" attribute entirely
         
-        //Using JavaScript's dataset property
-        var brand=mydiv.dataset.brand //returns "toyota"
-        mydiv.dataset.brand='mazda' //changes "data-brand" to "mazda"
-        mydiv.dataset.brand=null //removes "data-brand" attribute
-        */
-        var repo=d.dataset.repo;
-        var user=d.dataset.user;
+                //Using JavaScript's dataset property
+                var brand=mydiv.dataset.brand //returns "toyota"
+                mydiv.dataset.brand='mazda' //changes "data-brand" to "mazda"
+                mydiv.dataset.brand=null //removes "data-brand" attribute
+                */
+                var repo=d.dataset.repo;
+                var user=d.dataset.user;
 
-        githubAPI("repos/" + user + "/" + repo, function(data) {
-                repo = data.data;
-                //PROJECT NAME = repo.full_name
-                //...
-                ghdiv.children('h2.project-title').text(repo.full_name);
-                ghdiv.children('p.project-description').text(repo.description);
-                //ghdiv.children('h1.project-title').text(repo.full_name);
-                //ghdiv.children('h1.project-title').text(repo.full_name);
+                githubAPI("repos/" + user + "/" + repo, function(data) {
+                        repo = data.data;
+                        //PROJECT NAME = repo.full_name
+                        //...
+                        ghdiv.children('h2.project-title').text(repo.full_name);
+                        ghdiv.children('p.project-description').text(repo.description);
+                        //ghdiv.children('h1.project-title').text(repo.full_name);
+                        //ghdiv.children('h1.project-title').text(repo.full_name);
+                });
         });
-}
-);
 });
 
 function githubAPI(location, callback) {

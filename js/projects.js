b837553e3a01037cf9b2fc4d8e805561585e9514
Tumble <tumblegamer@tumblenet.tk>
2016-12-06 19:39:55 +0000

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -1,32 +1,36 @@
 
 ghdiv=$('div#github')
 
-var d=document.getElementById(ghdiv);
+ghdiv.each(function() {
+        var d=document.getElementById(this);
 
-/*
-var mydiv=document.getElementById('githun')
-//Using DOM's getAttribute() property
-var brand=mydiv.getAttribute("data-brand") //returns "toyota"
-mydiv.setAttribute("data-brand", "mazda") //changes "data-brand" to "mazda"
-mydiv.removeAttribute("data-brand") //removes "data-brand" attribute entirely
+        /*
+        var mydiv=document.getElementById('githun')
+        //Using DOM's getAttribute() property
+        var brand=mydiv.getAttribute("data-brand") //returns "toyota"
+        mydiv.setAttribute("data-brand", "mazda") //changes "data-brand" to "mazda"
+        mydiv.removeAttribute("data-brand") //removes "data-brand" attribute entirely
 
-//Using JavaScript's dataset property
-var brand=mydiv.dataset.brand //returns "toyota"
-mydiv.dataset.brand='mazda' //changes "data-brand" to "mazda"
-mydiv.dataset.brand=null //removes "data-brand" attribute
-*/
-var repo=d.dataset.repo;
-var user=d.dataset.user;
+        //Using JavaScript's dataset property
+        var brand=mydiv.dataset.brand //returns "toyota"
+        mydiv.dataset.brand='mazda' //changes "data-brand" to "mazda"
+        mydiv.dataset.brand=null //removes "data-brand" attribute
+        */
+        var repo=d.dataset.repo;
+        var user=d.dataset.user;
 
-githubAPI("repos/" + user + "/" + repo, function(data) {
-        repo = data.data;
-        //PROJECT NAME = repo.full_name
-        //...
-        ghdiv.children('h2.project-title').text(repo.full_name);
-        ghdiv.children('p.project-description').text(repo.description);
-        //ghdiv.children('h1.project-title').text(repo.full_name);
-        //ghdiv.children('h1.project-title').text(repo.full_name);
+        githubAPI("repos/" + user + "/" + repo, function(data) {
+                repo = data.data;
+                //PROJECT NAME = repo.full_name
+                //...
+                this.children('h2.project-title').text(repo.full_name);
+                this.children('p.project-description').text(repo.description);
+                //ghdiv.children('h1.project-title').text(repo.full_name);
+                //ghdiv.children('h1.project-title').text(repo.full_name);
+        });
+
 });
+
 
 function githubAPI(location, callback) {
   var url = "https://api.github.com/" + location + "?callback=?";

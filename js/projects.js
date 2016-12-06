2b078b385b6b5e45c633a04e252ae8eb6b228742
Tumble <tumblegamer@tumblenet.tk>
2016-12-06 15:42:02 +0000

    Update projects.js

--- js/projects.js
+++ js/projects.js
@@ -29,3 +29,8 @@ jQuery.fn.projects = function() {
                 
         //});
 }
+
+function githubAPI(location, callback) {
+  var url = "https://api.github.com/" + location + "?callback=?";
+  jQuery.getJSON(url,callback);
+}

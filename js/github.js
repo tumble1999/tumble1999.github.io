d7c7e626a46387d45d93047ff925f4e41d1f21f2
Tumble <tumblegamer@tumblenet.tk>
2016-12-12 21:49:05 +0000

    Update github.js

--- js/github.js
+++ js/github.js
@@ -1,6 +1,5 @@
 function githubAPI(location, callback) {
 	var jsondata;
-	var url = "https://api.github.com/" + location + "?client_id=d303c3ac5d6e32f036a2&client_secret=b27cf3e29e2654cd5b63e3c06ef54b6080883e14
-Revoke all user tokens&callback=?";
+	var url = "https://api.github.com/" + location + "?client_id=d303c3ac5d6e32f036a2&client_secret=b27cf3e29e2654cd5b63e3c06ef54b6080883e14&callback=?";
 	$.getJSON(url, callback);
 }

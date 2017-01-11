bc58f1697778fb4a6d3abb8efb42d9622558ff63
Tumble <tumblegamer@gmail.com>
2017-01-11 10:33:06 +0000

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,17 @@
 $(function() {
-alert("hello");
+var face = getQueryVariable('code');
+alert(face);
 window.location.replace("/");
 });
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
+
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+      }
+    }
+  }

4d69b1f3198ea99bcde1d54b0e06832199d62912
Tumble <tumblegamer@gmail.com>
2017-01-11 15:40:32 +0000

    Update login.js

--- js/login.js
+++ js/login.js
@@ -4,6 +4,7 @@ $(function() {
 	if (state=="bZ0TyBHhOCS6goHFQ8qj") {
 		var code = getQueryVariable('code');
 		alert(code);
+		acJson = getAccessCode("dce7328a9a7ea4492070", "32a13ccd5f5d63f86229d1df56b4e4893e48f8f4", code, state)
 	} else {
 		window.location.replace("https://github.com/login/oauth/authorize?client_id=dce7328a9a7ea4492070&allow_signup=true&redirect_uri=http://tumble1999.github.io/login&state=bZ0TyBHhOCS6goHFQ8qj");
 	}
@@ -24,7 +25,11 @@ function getQueryVariable(variable) {
   }
 
 function githubAPI(location, callback) {
-	var jsondata;
 	var url = "https://api.github.com/user?access_token=" +  + "&callback=?";
+	$.getJSON(url, callback);
+}
+
+function getAccessToken(clientId, clientSecret, code, state, callback) {
+	var url = "https://github.com/login/oauth/access_token?client_id=" + clientId + "&client_secret=" + clientSecret + "&code=" + code + "&state=" + state + "&callback=?";
 	$.getJSON(url, callback);
 }

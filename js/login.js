e997e118338f90b905a1616bf9047eb294a4a900
Tumble <tumblegamer@gmail.com>
2017-01-11 16:00:52 +0000

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,8 +2,8 @@ $(function() {
 	
 	var state = getQueryVariable('state');
 	if (state=="bZ0TyBHhOCS6goHFQ8qj") {
-		var authCode = getAuthCode(window.location.href);
-		var acJson = getAccessToken('dce7328a9a7ea4492070', '32a13ccd5f5d63f86229d1df56b4e4893e48f8f4', code, state);
+		var authCode = getQueryVariable('code');
+		//var acJson = getAccessToken('dce7328a9a7ea4492070', '32a13ccd5f5d63f86229d1df56b4e4893e48f8f4', code, state);
 		$.getJSON('https://tumble1999-comments.herokuapp.com/login/oauth/access_token'+code, function(data) {
 		 console.log(data.token);
 		});
@@ -20,3 +20,16 @@ function getAuthCode(url){
     }
     return url.match(/[&\?]code=([\w\/\-]+)/)[1];
 }
+
+function getQueryVariable(variable) {
+     var query = window.location.search.substring(1);
+     var vars = query.split('&');
+ 
+     for (var i = 0; i < vars.length; i++) {
+       var pair = vars[i].split('=');
+ 
+       if (pair[0] === variable) {
+         return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+       }
+     }
+   }

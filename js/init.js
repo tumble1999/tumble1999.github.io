2c259dd0d00a24cb4154e61a6e0ff8a22bf39a7d
Tumble <tumblegamer@gmail.com>
2017-01-12 12:28:00 +0000

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,4 +1,20 @@
 $(function() {
-
-
+  //LOCAL STORAGE USAGE
+  //var someVarName = localStorage.getItem("someVarName");
+  //localStorage.setItem("someVarName", someVarName);
+  
+  var loggedIn = localStorage.getItem("loggedIn");
+  if(loggedIn=="") {
+    loggedIn = "false";
+    localStorge.setItem("loggedIn", loggedIn);
+  }
+  
+  if(loggedIn=="false"){
+    $('#user-logged-in').removeClass("hidden");
+    $('#user-logged-out').addClass("hidden");
+  } else {
+    $('#user-logged-in').addClass("hidden");
+    $('#user-logged-out').removeClass("hidden");
+  }
+  
 });

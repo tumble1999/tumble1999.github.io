dc54fd3ffbbcc45aed0cfd0250f139cd90edefa9
Tumble <tumblegamer@gmail.com>
2017-01-12 11:32:08 +0000

    Update login.js

--- js/login.js
+++ js/login.js
@@ -4,6 +4,9 @@ $(function() {
 	if (state=="bZ0TyBHhOCS6goHFQ8qj") {
 		var authCode = getQueryVariable('code');
 		//var acJson = getAccessToken('dce7328a9a7ea4492070', '32a13ccd5f5d63f86229d1df56b4e4893e48f8f4', code, state);
+		$.getJSON('https://tumble1999-login.herokuapp.com/authenticate/'+code, function(data) {
+		 console.log(data.token);
+		});
 	} else {
 		window.location.replace('https://github.com/login/oauth/authorize?client_id=dce7328a9a7ea4492070&allow_signup=true&redirect_uri=http://tumble1999.github.io/login&state=bZ0TyBHhOCS6goHFQ8qj');
 	}

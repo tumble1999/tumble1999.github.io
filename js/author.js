2f5901dc7e53d19fd2e589f00922733d4e4bbcc0
Tumble <tumblegamer@gmail.com>
2017-01-12 12:38:20 +0000

    Update author.js

--- js/author.js
+++ js/author.js
@@ -1,4 +1,21 @@
 $(function() {
+	var loggedIn = localStorage.getItem("loggedIn");
+  var loggedInUser = localStorage.getItem("loggedInUser");
+  if(loggedIn=="") {
+    loggedIn = "false";
+    localStorge.setItem("loggedIn", loggedIn);
+  }
+  
+  $("#user-logged-in").attr("data-user", loggedInUser);
+  
+  if(loggedIn=="false"){
+    $('#user-logged-in').removeClass("hidden");
+    $('#user-logged-out').addClass("hidden");
+  } else {
+    $('#user-logged-in').addClass("hidden");
+    $('#user-logged-out').removeClass("hidden");
+  }
+	
 	var ghDiv;
 	var user;
 

ab26e85b8e93856124f7b49bdfbb58237d58bced
Tumble <tumblegamer@gmail.com>
2017-01-12 11:16:58 +0000

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,24 +2,12 @@ $(function() {
 	
 	var state = getQueryVariable('state');
 	if (state=="bZ0TyBHhOCS6goHFQ8qj") {
-		var authCode = getAuthCode(window.location.href);
+		var authCode = getQueryVariable('code');
 		//var acJson = getAccessToken('dce7328a9a7ea4492070', '32a13ccd5f5d63f86229d1df56b4e4893e48f8f4', code, state);
-		$.getJSON('https://tumble1999-comments.herokuapp.com/login/oauth/access_token'+code, function(data) {
-		 console.log(data.token);
-		});
 	} else {
 		window.location.replace('https://github.com/login/oauth/authorize?client_id=dce7328a9a7ea4492070&allow_signup=true&redirect_uri=http://tumble1999.github.io/login&state=bZ0TyBHhOCS6goHFQ8qj');
 	}
 });
-
-
-function getAuthCode(url){
-    var error = url.match(/[&\?]error=([^&]+)/);
-    if (error) {
-        throw 'Error getting authorization code: ' + error[1];
-    }
-    return url.match(/[&\?]code=([\w\/\-]+)/)[1];
-}
 
 function getQueryVariable(variable) {
      var query = window.location.search.substring(1);

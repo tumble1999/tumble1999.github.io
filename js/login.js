8c6347c524ce2d2c8e0fc50a1edef7afc68bf8ba
Tumble <tumblegamer@gmail.com>
2017-01-12 12:33:15 +0000

    Update login.js

--- js/login.js
+++ js/login.js
@@ -6,14 +6,14 @@ $(function() {
 		console.log(code);
 		$.getJSON('https://tumble1999-login.herokuapp.com/authenticate/'+code, function(data) {
 			$getJSON('https://api.github.com/user?access_token='+data.token+'&callback=?', function(data) {
-				 localStorge.setItem("loggedIn", "true");
-				 localStorge.setItem("loggedInUser", data.data.login);
+				localStorge.setItem("loggedIn", "true");
+				localStorge.setItem("loggedInUser", data.data.login);
+				window.location.replace('/');
 			 });
 		});
 	} else {
 		window.location.replace('https://github.com/login/oauth/authorize?client_id=dce7328a9a7ea4492070&allow_signup=true&redirect_uri=http://tumble1999.github.io/login&state=bZ0TyBHhOCS6goHFQ8qj');
 	}
-	window.location.replace('/');
 });
 
 function getQueryVariable(variable) {

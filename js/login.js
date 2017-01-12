1672e4e4ebe4eb5791da828a8f1198d28123df02
Tumble <tumblegamer@gmail.com>
2017-01-12 12:34:41 +0000

    Update login.js

--- js/login.js
+++ js/login.js
@@ -5,7 +5,7 @@ $(function() {
 		var code = getQueryVariable('code');
 		console.log(code);
 		$.getJSON('https://tumble1999-login.herokuapp.com/authenticate/'+code, function(data) {
-			$getJSON('https://api.github.com/user?access_token='+data.token+'&callback=?', function(data) {
+			$.getJSON('https://api.github.com/user?access_token='+data.token+'&callback=?', function(data) {
 				localStorge.setItem("loggedIn", "true");
 				localStorge.setItem("loggedInUser", data.data.login);
 				window.location.replace('/');

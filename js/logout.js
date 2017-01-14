612fa9516ed9ea4c06128013890d8cff0bb18201
Tumble <tumblegamer@gmail.com>
2017-01-14 13:11:38 +0000

    Update logout.js

--- js/logout.js
+++ js/logout.js
@@ -1,5 +1,7 @@
 $(function() {
     localStorage.setItem("loggedIn", "false");
+    localStorage.setItem("access_token", "");
+    localStorage.setItem("ref", "");
     //window.location.replace('/');
     history.back()
 });

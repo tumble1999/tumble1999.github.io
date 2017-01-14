6c0d531f53f15dc471e6fc15b5da260e93237703
Tumble <tumblegamer@gmail.com>
2017-01-14 13:03:58 +0000

    Update author.js

--- js/author.js
+++ js/author.js
@@ -1,59 +1,68 @@
 $(function() { 
-	
+	var access_token;
+	getUserInfo(function() {
+		var ghDiv;
+		var user;
+
+		//console.log(ghDivs);
+
+		$('.githubAuthor').each(function(id, object) {
+			console.log(object);
+			console.log("USER:" + this.dataset.user);
+			githubAPI("users/" + this.dataset.user,
+				function(userInfo) {
+					console.log(userInfo.data);
+
+					$(object).find('.userName').text(userInfo.data.name);
+					$(object).find('.userImg').attr("src", userInfo.data.avatar_url);
+				});
+
+			/* githubAPI("repos/" + user + "/" + repo, function(data) {
+				console.log("REPO: " + repo + "   USER: " + user);
+				repoInfo = data.data;
+				console.log("REPO_INFO");
+				console.log(repoInfo);
+				projectTitle = repoInfo.full_name;
+				projectDescription = repoInfo.description;
+				console.log(ghDiv.children(".projectTitle").text(projectTitle));
+			});*/
+
+		});
+	});
+});
+
+function getUserInfo(callback) {
+	var url;
 	var loggedIn = localStorage.getItem("loggedIn");
 	console.log(access_token);
 	if (loggedIn=="true") {
-		var access_token = localStorage.getItem("access_token");
-		var url = "https://api.github.com/user?access_token=" + access_token;
-		$.getJSON(url, function(currentUser) {
-	 		var loggedInUser = currentUser.login;
-			console.log(loggedInUser);
-			$('#user-logged-in').attr("data-user", loggedInUser);
-			$('#user-logged-in > img').attr("src", "https://github.com/identicons/" + loggedInUser + ".png");
-			$('.loggedInUserProfile').attr("href", "http://github.com/" + loggedInUser);
-			
-			if(loggedIn=="true"){
-		    		$('.user-logged-in, #user-logged-in').show();
-		    		$('.user-logged-out, #user-logged-out').hide();
-		  	} else {
-		    		$('.user-logged-in, #user-logged-in').hide();
-		    		$('.user-logged-out, #user-logged-out').show();
-		  	}
-			$('.newCommentUsername').val(loggedInUser);
-			$('.newCommentUsername').attr("value", loggedInUser );
-			$('.newCommentUsername').parent().addClass('is-dirty');
-		});
-		
+		access_token = localStorage.getItem("access_token");
+		url = "https://api.github.com/user?access_token=" + access_token;
+	} else {
+		access_token = "";
+		url = "";
 	}
-
 	
-	
-	var ghDiv;
-	var user;
-	
-	//console.log(ghDivs);
+	$.getJSON(url, function(currentUser) {
+		var loggedInUser = currentUser.login;
+		console.log(loggedInUser);
+		$('#user-logged-in').attr("data-user", loggedInUser);
+		$('#user-logged-in > img').attr("src", "https://github.com/identicons/" + loggedInUser + ".png");
+		$('.loggedInUserProfile').attr("href", "http://github.com/" + loggedInUser);
 
-	$('.githubAuthor').each(function(id, object) {
-		console.log(object);
-		console.log("USER:" + this.dataset.user);
-		githubAPI("users/" + this.dataset.user,
-			function(userInfo) {
-				console.log(userInfo.data);
-
-				$(object).find('.userName').text(userInfo.data.name);
-				$(object).find('.userImg').attr("src", userInfo.data.avatar_url);
-			});
-
-		/* githubAPI("repos/" + user + "/" + repo, function(data) {
-			console.log("REPO: " + repo + "   USER: " + user);
-			repoInfo = data.data;
-			console.log("REPO_INFO");
-			console.log(repoInfo);
-			projectTitle = repoInfo.full_name;
-			projectDescription = repoInfo.description;
-			console.log(ghDiv.children(".projectTitle").text(projectTitle));
-		});*/
-
+		if(loggedIn=="true"){
+			$('.user-logged-in, #user-logged-in').show();
+			$('.user-logged-out, #user-logged-out').hide();
+		} else {
+			$('.user-logged-in, #user-logged-in').hide();
+			$('.user-logged-out, #user-logged-out').show();
+		}
+		$('.newCommentUsername').val(loggedInUser);
+		$('.newCommentUsername').attr("value", loggedInUser );
+		$('.newCommentUsername').parent().addClass('is-dirty');
+		callback();
 	});
-});
+		
+	
+}
 

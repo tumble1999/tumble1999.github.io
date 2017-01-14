b93172f0c341147681c3c4434ab3acf9c65d36b5
Tumble <tumblegamer@gmail.com>
2017-01-14 17:43:12 +0000

    Update author.js

--- js/author.js
+++ js/author.js
@@ -6,58 +6,83 @@ $(function() {
 	var url = "";
 	var admins_url = "";
 	
-	UserInit(function() {
-		refresh();
-	});
-});
-function UserInit(callback) {
-	loggedIn = localStorage.getItem("loggedIn");
-	console.log("Logged In: " + loggedIn);
-	if(loggedIn=="true") {
-		userLoggedIn(callback());
-	} else {
-		userLoggedOut();
-		callback();
+	UserInit(refresh());
+	
+	function UserInit(callback) {
+		loggedIn = localStorage.getItem("loggedIn");
+		console.log("Logged In: " + loggedIn);
+		if(loggedIn=="true") {
+			userLoggedIn(callback());
+		} else {
+			userLoggedOut();
+			callback();
+		}
 	}
-}
 
-function userLoggedIn(callback) {
-	access_token = localStorage.getItem("access_token");
-	url = "https://api.github.com/user?access_token=" + access_token;
-	var admin_code_1 = "ab85ff5428f26f488cd0";
-	var admin_code_2 = "3d7057b8ee5e536a4d06";
-	admins_url = "https://api.github.com/repos/tumble1999/tumble1999.github.io/collaborators?access_token=" + admin_code_1 + admin_code_2;
-	getUserInfo(url, admins_url, function() {
-		$('.user-logged-in, #user-logged-in').show();
-		$('.user-logged-out, #user-logged-out').hide();
-		$('#user-logged-in').attr("data-user", loggedInUser);
-		$('#user-logged-in > img').attr("src", "https://github.com/identicons/" + loggedInUser + ".png");
-		$('.loggedInUserProfile').attr("href", "http://github.com/" + loggedInUser);
+	function userLoggedIn(callback) {
+		access_token = localStorage.getItem("access_token");
+		url = "https://api.github.com/user?access_token=" + access_token;
+		var admin_code_1 = "ab85ff5428f26f488cd0";
+		var admin_code_2 = "3d7057b8ee5e536a4d06";
+		admins_url = "https://api.github.com/repos/tumble1999/tumble1999.github.io/collaborators?access_token=" + admin_code_1 + admin_code_2;
+		getUserInfo(url, admins_url, function() {
+			$('.user-logged-in, #user-logged-in').show();
+			$('.user-logged-out, #user-logged-out').hide();
+			$('#user-logged-in').attr("data-user", loggedInUser);
+			$('#user-logged-in > img').attr("src", "https://github.com/identicons/" + loggedInUser + ".png");
+			$('.loggedInUserProfile').attr("href", "http://github.com/" + loggedInUser);
 
-		$('.newCommentUsername').val(loggedInUser);
-		$('.newCommentUsername').attr("value", loggedInUser );
-		$('.newCommentUsername').parent().addClass('is-dirty');
-		callback();
+			$('.newCommentUsername').val(loggedInUser);
+			$('.newCommentUsername').attr("value", loggedInUser );
+			$('.newCommentUsername').parent().addClass('is-dirty');
+			callback();
+		});
+	}
+	function userLoggedOut() {
+		access_token = "";
+		url = "";
+		admins_url = "";
+
+		loggedIn = "false";
+		localStorage.setItem("loggedIn", loggedIn);
+		$('.user-logged-in, #user-logged-in').hide();
+		$('.user-logged-out, #user-logged-out').show();
+	}
+	function userIsNotAdmin() {
+		$('.user-admin-only, #user-admin-only').hide();
+		//$('.user-logged-out, #user-logged-out').show();
+	}
+	function userIsAdmin() {
+		$('.user-admin-only, #user-admin-only').show();
+		//$('.user-logged-out, #user-logged-out').hide();
+	}
+	function getUserInfo(url, admins_url, callback) {
+	$.getJSON(url, function(currentUser) {
+		loggedInUser = currentUser.login;
+		console.log("Logged in user: " + loggedInUser);
+
+		$.getJSON(admins_url, function(colabs) {
+			//console.log(colabs);
+			for(var i = 0; i < colabs.length; i++) {
+			    if (colabs[i].login == loggedInUser) {
+				admin = true;
+				break;
+			    }
+			}
+			console.log("ADMIN: " + admin);
+			if (admin) {
+				userIsAdmin();
+			} else {
+				userIsNotAdmin();
+			}
+			callback();
+		});
 	});
-}
-function userLoggedOut() {
-	access_token = "";
-	url = "";
-	admins_url = "";
 
-	loggedIn = "false";
-	localStorage.setItem("loggedIn", loggedIn);
-	$('.user-logged-in, #user-logged-in').hide();
-	$('.user-logged-out, #user-logged-out').show();
+
 }
-function userIsNotAdmin() {
-	$('.user-admin-only, #user-admin-only').hide();
-	//$('.user-logged-out, #user-logged-out').show();
-}
-function userIsAdmin() {
-	$('.user-admin-only, #user-admin-only').show();
-	//$('.user-logged-out, #user-logged-out').hide();
-}
+});
+
 
 function refresh() {		
 	var ghDiv;
@@ -87,30 +112,25 @@ function refresh() {
 	});
 }
 
-function getUserInfo(url, admins_url, callback) {
-	$.getJSON(url, function(currentUser) {
-		loggedInUser = currentUser.login;
-		console.log("Logged in user: " + loggedInUser);
+function amIAnAdmin() {
+	if(loggedIn == "true") {
+		if(admin) {
+			console.log("Yes" + loggedInUser +  ", you are!");
+		} else {
+			console.log("No" + loggedInUser +  ", stop begging.");
+		}
+	} else {
+		console.log("How would I know, your not even logged in!");
+		console.log("You might as well be asking `amILoggedIn()`.");
+	}
+}
 
-		$.getJSON(admins_url, function(colabs) {
-			//console.log(colabs);
-			for(var i = 0; i < colabs.length; i++) {
-			    if (colabs[i].login == loggedInUser) {
-				admin = true;
-				break;
-			    }
-			}
-			console.log("ADMIN: " + admin);
-			if (admin) {
-				userIsAdmin();
-			} else {
-				userIsNotAdmin();
-			}
-			callback();
-		});
-	});
-
-
+function amILoggedIn() {
+	if(loggedIn == "true") {
+		console.log("Yes.  Welcome " + loggedInUser + ".");
+	} else {
+		console.log("No. Go away strager");
+	}
 }
 
 

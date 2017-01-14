56b354ca27d801106156673e02b2c0109eb180c1
Tumble <tumblegamer@gmail.com>
2017-01-14 18:43:29 +0000

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,7 +1,9 @@
-$(function() {
+var UnderConstruction == true;
+(function() {
   //LOCAL STORAGE USAGE
   //var someVarName = localStorage.getItem("someVarName");
   //localStorage.setItem("someVarName", someVarName);
+  
   
   
   

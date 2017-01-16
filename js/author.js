2a0073c59c16004c60a7f49a8dc48cf5fc3b5365
Tumble <tumblegamer@gmail.com>
2017-01-16 11:38:50 +0000

    Update author.js

--- js/author.js
+++ js/author.js
@@ -20,7 +20,12 @@ $(function() {
 	}
 
 	function userLoggedIn(callback) {
-		function uliP2(callback){
+		access_token = localStorage.getItem("access_token");
+		url = "https://api.github.com/user?access_token=" + access_token;
+		var admin_code_1 = "ab85ff5428f26f488cd0";
+		var admin_code_2 = "3d7057b8ee5e536a4d06";
+		admins_url = "https://api.github.com/repos/tumble1999/tumble1999.github.io/collaborators?access_token=" + admin_code_1 + admin_code_2;
+		getUserInfo(url, admins_url, function() {
 			$('.user-logged-in, #user-logged-in').show();
 			$('.user-logged-out, #user-logged-out').hide();
 			$('#user-logged-in').attr("data-user", loggedInUser);
@@ -30,14 +35,8 @@ $(function() {
 			$('.newCommentUsername').val(loggedInUser);
 			$('.newCommentUsername').attr("value", loggedInUser );
 			$('.newCommentUsername').parent().addClass('is-dirty');
-			callback;
-		}
-		access_token = localStorage.getItem("access_token");
-		url = "https://api.github.com/user?access_token=" + access_token;
-		var admin_code_1 = "ab85ff5428f26f488cd0";
-		var admin_code_2 = "3d7057b8ee5e536a4d06";
-		admins_url = "https://api.github.com/repos/tumble1999/tumble1999.github.io/collaborators?access_token=" + admin_code_1 + admin_code_2;
-		getUserInfo(url, admins_url, uliP2(callback));
+			callback;			
+		});
 	}
 	function userLoggedOut() {
 		access_token = "";
@@ -76,7 +75,7 @@ $(function() {
 			} else {
 				userIsNotAdmin();
 			}
-			callback();
+			callback;
 		});
 	});
 

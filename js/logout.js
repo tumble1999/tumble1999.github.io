4ce6c883c11c8781d1b043f2ba19d46b24014564
Tumble <tumblegamer@gmail.com>
2017-01-16 17:51:50 +0000

    Update

--- js/logout.js
+++ js/logout.js
@@ -1,7 +1,7 @@
 $(function() {
     localStorage.clear();
     //window.location.replace('/');
-    window.open("http:/github.com/logout");
+    window.open("https://github.com/logout");
     history.back()
 });
 

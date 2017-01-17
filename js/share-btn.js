31f7482b3b32fdf96dc94697ce5bb1a64391453f
Tumble <tumblegamer@gmail.com>
2017-01-17 10:23:25 +0000

    Update share-btn.js

--- js/share-btn.js
+++ js/share-btn.js
@@ -1,7 +1,10 @@
 var shareDialog = document.querySelector('dialog.dialogShare');
-var showModalButton = document.querySelector('.show-modal');
-if (! dialog.showModal) {
-  dialogPolyfill.registerDialog(dialog);
+var shareButton = document.querySelector('.shareButton');
+ shareButton.addEventListener('click', function() {
+    shareDialog.showModal();
+  });
+if (! shareDialog.showModal) {
+  dialogPolyfill.registerDialog(shareDialog);
 }
 
 

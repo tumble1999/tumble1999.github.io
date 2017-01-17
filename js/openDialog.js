c638fd7707c03837384707f1d244fca63648ef7b
Tumble <tumblegamer@gmail.com>
2017-01-17 15:22:14 +0000

    Create openDialog.js

---
+++ js/openDialog.js
@@ -0,0 +1,5 @@
+openDialog = function(title, msg, height, width, linkTitles, linkUrls) {
+
+
+window.open("/dialog?title=" + title + "&text=" + msg, "", "status=0,toolbar=0,location=0,menubar=0,directories=0,resizeable=0,scrollbars=0,width=400,height=300")
+}

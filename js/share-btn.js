05648412e829d09fe40a90016c4d6c60e5ebdb83
Tumble <tumblegamer@gmail.com>
2017-01-17 10:32:07 +0000

    Update share-btn.js

--- js/share-btn.js
+++ js/share-btn.js
@@ -1,11 +1,13 @@
-var shareDialog = document.querySelector('dialog.dialogShare');
-var shareButton = document.querySelector('#shareButton');
-if (! shareDialog.showModal) {
-  dialogPolyfill.registerDialog(shareDialog);
-}
-shareButton.addEventListener('click', function() {
-   shareDialog.showModal();
- });
-shareDialog.querySelector('.doneButton').addEventListener('click', function() {
-  shareDialog.close();
+$(function() {
+  var shareDialog = document.querySelector('dialog.dialogShare');
+  var shareButton = document.querySelector('#shareButton');
+  if (! shareDialog.showModal) {
+    dialogPolyfill.registerDialog(shareDialog);
+  }
+  shareButton.addEventListener('click', function() {
+     shareDialog.showModal();
+   });
+  shareDialog.querySelector('.doneButton').addEventListener('click', function() {
+    shareDialog.close();
+  });
 });

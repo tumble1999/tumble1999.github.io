3694b1b1ecb6f17d45cb7b466b0105e5eebe42c3
Tumble <tumblegamer@gmail.com>
2018-10-29 11:40:54 +0000

    Update github.js

--- js/github.js
+++ js/github.js
@@ -1,7 +1,7 @@
 function githubAPI(location, callback) {
 	var jsondata;
-	var token1 = "1c5d031cbdcf02e19998";
-	var token2 = "efe7d31418efdfa6fcde";
+	var token1 = "35fd416d3251f72db01b";
+	var token2 = "d635bdb41770da4f9f61";
 	var url = "https://api.github.com/" + location + "?access_token=" + token1 + token2 + "&callback=?";
 	$.getJSON(url, callback);
 }

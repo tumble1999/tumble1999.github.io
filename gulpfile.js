cb655812a71ad24b053510ab501c4fa5ab8aa069
Tumble <15913224+tumble1999@users.noreply.github.com>
2021-10-13 00:51:05 +0100

    maparino

--- gulpfile.js
+++ gulpfile.js
@@ -17,18 +17,18 @@ function js(cb) {
     uglify(),
     concat("tumble.min.js"),
     sourcemaps.write("./",{sourceRoot:smRoot+"/js"}),
-	prepend(`/**
-* Author: Cameron Trow
-* License: GPL-3
-* Source Code: https://github.com/tumble1999/tumble1999.github.io
-* 
-* You may copy, distribute and modify the software as long as you
-* track changes/dates in source files. Any modifications to or
-* software including (via compiler) GPL-licensed code must also
-* be made available under the GPL along with build & install
-* instructions.
-*/
-`),
+// 	prepend(`/**
+// * Author: Cameron Trow
+// * License: GPL-3
+// * Source Code: https://github.com/tumble1999/tumble1999.github.io
+// * 
+// * You may copy, distribute and modify the software as long as you
+// * track changes/dates in source files. Any modifications to or
+// * software including (via compiler) GPL-licensed code must also
+// * be made available under the GPL along with build & install
+// * instructions.
+// */
+// `),
     gulp.dest("assets/js"),
     cb
   );
@@ -41,18 +41,18 @@ function css(cb) {
     sass({ outputStyle: "compressed" }).on("error", sass.logError),
     concat("tumble.min.css"),
     sourcemaps.write("./",{sourceRoot:smRoot+"/scss"}),
-	prepend(`/**
-* Author: Cameron Trow
-* License: GPL-3
-* Source Code: https://github.com/tumble1999/tumble1999.github.io
-* 
-* You may copy, distribute and modify the software as long as you
-* track changes/dates in source files. Any modifications to or
-* software including (via compiler) GPL-licensed code must also
-* be made available under the GPL along with build & install
-* instructions.
-*/
-`),
+// 	prepend(`/**
+// * Author: Cameron Trow
+// * License: GPL-3
+// * Source Code: https://github.com/tumble1999/tumble1999.github.io
+// * 
+// * You may copy, distribute and modify the software as long as you
+// * track changes/dates in source files. Any modifications to or
+// * software including (via compiler) GPL-licensed code must also
+// * be made available under the GPL along with build & install
+// * instructions.
+// */
+// `),
     gulp.dest("assets/css"),
     cb
   );
